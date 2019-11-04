6029a3edb86dc7ae40e7432085fd2ede58632848
Serhii Vekshyn <serhii.vekshyn@hotline.finance>
2019-11-04 02:04:10 +0200

    created frontend

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,44 @@
+import React, { useState, Component } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Container
+} from 'reactstrap';
+
+class AppNavbar extends Component {
+    state = {
+        isOpen: false
+    }
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <Navbar color="dark" dark expand={'sm'} className={'mb-5'}>
+                    <Container>
+                        <NavbarBrand href="/" className="mr-auto">my MERN app</NavbarBrand>
+                        <NavbarToggler onClick={this.toggle} className="mr-2" />
+                        <Collapse isOpen={this.state.isOpen} navbar>
+                            <Nav className={'ml-auto'} navbar>
+                                <NavItem>
+                                    <NavLink href="https://github.com/serhiiVek/mern-story-app">GitHub</NavLink>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
+                    </Container>
+                </Navbar>
+            </div>
+        );
+    }
+}
+
+export default AppNavbar;
